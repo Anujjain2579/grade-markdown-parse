632c2064f8e302fd67514bad05e938bed97d166b
Anujjain2579 <103171741+Anujjain2579@users.noreply.github.com>
2022-05-06 15:54:57 -0700

    testing file existence
    If statement added

--- grade.sh
+++ grade.sh
@@ -1,9 +1,9 @@
 
 git clone $1
+if test -f "$1"; then 
+    echo "$1 exists."
+fi
 cp GradingTests.java markdown-parser/
 cd markdown-parser
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
-if test -f "$1"; then 
-    echo "$1 exists."
-fi
